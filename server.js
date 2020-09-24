a26cdd54e84c61c4a1d8d328799d286e8f10aeb1
isaacoduh <ufuomaoduh@gmail.com>
2020-09-24 02:21:33 +0100

    xx-read-update-delete-dishes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./models');
+const dish = require('./models/dish');
 
 const app = express();
 
@@ -20,18 +21,42 @@ app.get('/api/dishes', (req, res) => {
 } );
 
 app.post('/api/dishes', (req, res) => {
-    // TODO: create a dish 
+    // TODO: create a dish
+    const {title, description, price} = req.body;
+    return db.Dish.create({title, description, price})
+        .then((dish) => res.send(dish))
+        .catch((err) => {
+            console.log('***There was an error creating a new dish', JSON.stringify(err))
+            return res.send(err)
+        });
 });
 
 app.delete('/api/dishes/:id', (req, res) => {
-    const id = parseInt(req.params.id);
     // TODO: find and delete dish by id
+    const id = parseInt(req.params.id);
+    return db.Dish.findByPk(id)
+    .then((dish) => dish.destroy())
+    .then(() => res.send({id}))
+    .catch((err) => {
+        console.log('***Error Deleting dish information', JSON.stringify(err))
+        res.status(400).send(err)
+    })
 });
 
 app.put('/api/dishes/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const {name, description, price} = req.body
     // TODO: find and update dish by id 
+    const id = parseInt(req.params.id);
+    return db.Dish.findByPk(id)
+    .then((dish) => {
+        const {title, description, price} = req.body
+        return dish.update({title, description, price})
+            .then(() => res.send(dish))
+            .catch((err) => {
+                console.log('***Error Updating dish information', JSON.stringify(err))
+                res.status(400).send(err)
+            })
+    })
+
 });
 
 app.listen(3000, () => {
